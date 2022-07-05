9d321ca6de875e22a210991b6ab29ccd506816b5
nguyen_minh_nhut <minhnhut.devcntt@gmail.com>
2022-07-05 20:34:46 +0700

    Add KyC

--- ProjectResearch/Stablecoin/example/truffle/contracts/MyTokenSale.sol
+++ ProjectResearch/Stablecoin/example/truffle/contracts/MyTokenSale.sol
@@ -1,12 +1,29 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.2;
 
 import "./Crowdsale.sol";
+import "./KycContract.sol";
 
 contract MyTokenSale is Crowdsale {
+    KycContract kyc;
 
     constructor(
         uint256 rate, // rate in TKNbits
         address payable wallet,
-        IERC20 token
-    ) public Crowdsale(rate, wallet, token) {}
+        IERC20 token,
+        KycContract _kyc
+    ) public Crowdsale(rate, wallet, token) {
+        kyc = _kyc;
+    }
+
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount)
+        internal
+        view
+        override
+    {
+        super._preValidatePurchase(beneficiary, weiAmount);
+        require(
+            kyc.kycCompleted(beneficiary),
+            "KYC not completed yet, aborting"
+        );
+    }
 }
--- ProjectResearch/Stablecoin/example/truffle/migrations/2_deploy_example.js
+++ ProjectResearch/Stablecoin/example/truffle/migrations/2_deploy_example.js
@@ -1,5 +1,7 @@
 var MyToken = artifacts.require("MyToken.sol");
 var MyTokenSales = artifacts.require("MyTokenSale.sol");
+var KycContract = artifacts.require("KycContract.sol");
+
 require('dotenv').config({path: `${__dirname}/../../.env`});
 
 
@@ -7,7 +9,9 @@ module.exports = async function(deployer) {
     let addr = await web3.eth.getAccounts();
     //await deployer.deploy(MyToken, 1000000000);
     await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
-    await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address);
+    await deployer.deploy(KycContract);
+    await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address, KycContract.address);
+    //await deployer.deploy(MyTokenSales, 1, addr[0], MyToken.address);
     let tokenInstance = await MyToken.deployed();
     //await tokenInstance.transfer(MyTokenSales.address, 1000000000);
     await tokenInstance.transfer(MyTokenSales.address, process.env.INITIAL_TOKENS);
